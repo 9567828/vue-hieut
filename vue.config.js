fc4e59c4bdc034ba807d8b6974e2e0d921d70c60
9567828 <kimssi9567@gmail.com>
2025-03-02 16:46:50 +0900

    '오타수정'

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,7 @@ const webpack = require("webpack");
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: "/vue-hieut/",
   configureWebpack: {
     plugins: [
       new webpack.DefinePlugin({
